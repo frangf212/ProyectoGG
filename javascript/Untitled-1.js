e6f86ada655ecf49eee78202c25489bc22516d05
Francisco Grande <grandefrancisco43@gmail.com>
2026-02-06 09:36:43 +0100

    cambios e implementación de javascrit

---
+++ javascript/Untitled-1.js
@@ -0,0 +1,14 @@
+document.getElementById("boton_busqueda").onclick = function() {
+    alert("Debes iniciar sesión primero");
+};
+
+
+
+
+
+
+
+
+document.getElementById("botonregistro").onclick = function() {
+alert("Cuenta creada con exito");
+};
